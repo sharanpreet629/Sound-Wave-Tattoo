a7eeb7a09141d98dc76762b4cb3a27bfd6f34530
Sharanpreet Kaur <83025267+sharanpreet629@users.noreply.github.com>
2022-03-28 14:51:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,109 +42,109 @@ session = Session()
 
 #user defined functions
 def read_audio(file_name):
-    global fname
-    fname = file_name.split('.')[0]
-    global audio
-    audio = wave.open(file_name,'r')
-    return audio, fname
+	global fname
+	fname = file_name.split('.')[0]
+	global audio
+	audio = wave.open(file_name,'r')
+	return audio, fname
 
 def plot_audio(name):
-    #color = input('Which color do you want to plot(r/g/b): ')
-    global signal_byte
-    signal_byte = audio.readframes(-1)
-    signal = np.fromstring(signal_byte, 'int16')
-    freq = audio.getframerate()
+	#color = input('Which color do you want to plot(r/g/b): ')
+	global signal_byte
+	signal_byte = audio.readframes(-1)
+	signal = np.fromstring(signal_byte, 'int16')
+	freq = audio.getframerate()
 
-    time = np.linspace(0, len(signal)/freq, num=len(signal))
+	time = np.linspace(0, len(signal)/freq, num=len(signal))
 
-    plt.figure(1, figsize=(10,9))
-    plt.plot(time,signal)
-    plt.axis('off')
-    print(name)
-    namel = name.split('/')[1:]
-    random_key = namel[-1]
-    path = '/static/{}.png'.format(random_key)
-    print(path)
-    plt.savefig(path)
-    #plt.show()
-    name = str(random_key)+'.png'
-    return name, path,random_key
+	plt.figure(1, figsize=(10,9))
+	plt.plot(time,signal)
+	plt.axis('off')
+	print(name)
+	namel = name.split('/')[1:]
+	random_key = namel[-1]
+	path = '/static/{}.png'.format(random_key)
+	print(path)
+	plt.savefig(path)
+	#plt.show()
+	name = str(random_key)+'.png'
+	return name, path,random_key
 
 def write_text(path, unique_key):
-    img = Image.open(path)
-    img1 = ImageDraw.Draw(img)
-    myFont = ImageFont.truetype('FreeMono.ttf',40)
-    img1.text((450,800),unique_key,fill=(125,125,125),font=myFont)
-    img.save(path)
+	img = Image.open(path)
+	img1 = ImageDraw.Draw(img)
+	myFont = ImageFont.truetype('FreeMono.ttf',40)
+	img1.text((450,800),unique_key,fill=(125,125,125),font=myFont)
+	img.save(path)
 
 def text_detection(path):
-    documentName = path
-    with open(documentName, 'rb') as document:
-        imageBytes = bytearray(document.read())
+	documentName = path
+	with open(documentName, 'rb') as document:
+	imageBytes = bytearray(document.read())
 
-    client = boto3.client('textract',
-                          region_name='us-east-1',
-                          aws_access_key_id=ACCESS_KEY,
-                          aws_secret_access_key=SECRET_KEY)
+	client = boto3.client('textract',
+			  region_name='us-east-1',
+			  aws_access_key_id=ACCESS_KEY,
+			  aws_secret_access_key=SECRET_KEY)
 
-    response = client.detect_document_text(Document={'Bytes': imageBytes})
-    blocks = response['Blocks']
-    all_lines = [l for l in blocks if l['BlockType'] == 'LINE']
-    for l in all_lines:
-        global Key
-        Key = l['Text']
-        print(Key)
-    return Key
+	response = client.detect_document_text(Document={'Bytes': imageBytes})
+	blocks = response['Blocks']
+	all_lines = [l for l in blocks if l['BlockType'] == 'LINE']
+	for l in all_lines:
+	global Key
+	Key = l['Text']
+	print(Key)
+	return Key
 
 def image2db(path, key):
-    name = fname
-    gis = ImageSignature()
-    image_key = gis.generate_signature(path)
-    image_sig = str(list(image_key))
-    audio_b = signal_byte
-    parameters = audio.getparams()
-    nchannels = parameters[0]
-    sampwidth = parameters[1]
-    framerate = parameters[2]
-    nframes = parameters[3]
-    comptype = parameters[4]
-    compname = parameters[5]
-    instance = Audio(name = name, key = key,  image_signature = image_sig, \
-    audio_bytes = audio_b, nchannels = nchannels, sampwidth = sampwidth, \
-    framerate = framerate, nframes = nframes, comptype = comptype, compname = compname) # create an audio instance
+	name = fname
+	gis = ImageSignature()
+	image_key = gis.generate_signature(path)
+	image_sig = str(list(image_key))
+	audio_b = signal_byte
+	parameters = audio.getparams()
+	nchannels = parameters[0]
+	sampwidth = parameters[1]
+	framerate = parameters[2]
+	nframes = parameters[3]
+	comptype = parameters[4]
+	compname = parameters[5]
+	instance = Audio(name = name, key = key,  image_signature = image_sig, \
+	audio_bytes = audio_b, nchannels = nchannels, sampwidth = sampwidth, \
+	framerate = framerate, nframes = nframes, comptype = comptype, compname = compname) # create an audio instance
 
-    session.add(instance) # add to database
-    session.commit()
+	session.add(instance) # add to database
+	session.commit()
 
 def find_distances(image):
-    im_key = text_detection(image)
-    print(im_key)
-    global all_signatures
-    all_signatures = session.query(Audio.key).all()
-    global distances
-    listy = [signature[0] for signature in all_signatures]
-    print(listy)
-    if im_key in listy:
-        index = listy.index(im_key)
-        audio_bytes, nchannels, sampwidth, framerate, nframes, comptype, compname = \
-            session.query(Audio.audio_bytes, Audio.nchannels, Audio.sampwidth, \
-                          Audio.framerate, Audio.nframes, Audio.comptype, Audio.compname).filter(Audio.id \
-                          == index + 1).first()
-        file_path = '/static/playback_test.wav'
-        tune = wave.open(file_path , 'wb')
-        tune.setnchannels(nchannels)
-        tune.setsampwidth(sampwidth)
-        tune.setframerate(framerate)
-        tune.setnframes(nframes)
-        tune.setcomptype(comptype, compname)
-        tune.writeframes(audio_bytes)
-        tune.close()
-        AudioSegment.from_wav(file_path)
-        return print('Done')
-    else:
-        return print('Not Found')
+	im_key = text_detection(image)
+	print(im_key)
+	global all_signatures
+	all_signatures = session.query(Audio.key).all()
+	global distances
+	listy = [signature[0] for signature in all_signatures]
+	print(listy)
+	if im_key in listy:
+	index = listy.index(im_key)
+	audio_bytes, nchannels, sampwidth, framerate, nframes, comptype, compname = \
+	    session.query(Audio.audio_bytes, Audio.nchannels, Audio.sampwidth, \
+			  Audio.framerate, Audio.nframes, Audio.comptype, Audio.compname).filter(Audio.id \
+			  == index + 1).first()
+	file_path = '/static/playback_test.wav'
+	tune = wave.open(file_path , 'wb')
+	tune.setnchannels(nchannels)
+	tune.setsampwidth(sampwidth)
+	tune.setframerate(framerate)
+	tune.setnframes(nframes)
+	tune.setcomptype(comptype, compname)
+	tune.writeframes(audio_bytes)
+	tune.close()
+	AudioSegment.from_wav(file_path)
+	return print('Done')
+	else:
+	return print('Not Found')
 
-    return file_path
+	return file_path
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif','wav'])
 def allowed_file(filename):
@@ -152,7 +152,7 @@ def allowed_file(filename):
 
 @app.route('/', methods=['GET'])
 def Home():
-    return "Hello World"
+	return "Hello World"
 
 @app.route('/uploader', methods=['GET', 'POST'])
 def upload_file():
