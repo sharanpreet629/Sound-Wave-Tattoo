886d433d7e2f1c88b3fc566ace31ce700780d519
Sharanpreet Kaur <83025267+sharanpreet629@users.noreply.github.com>
2022-03-28 15:30:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ def Home():
 @app.route('/uploader', methods=['GET', 'POST'])
 def upload_file():
 	if request.method == 'POST':
-		file = request.files['']
+		file = request.files['file']
 		print(file)
 		if file.filename == '':
 			resp = jsonify({'message': 'No file selected for uploading'})
