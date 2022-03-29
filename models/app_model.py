0368d40a812aa82ef79d9e84ba3975ff9d359756
Sharanpreet Kaur <83025267+sharanpreet629@users.noreply.github.com>
2022-03-29 16:31:55 +0530

    Delete models directory

--- models/app_model.py
+++
@@ -1,8 +0,0 @@
-from db.db import db
-import datetime
-
-
-class Profile(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    img_name = db.Column(db.String(255), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow, nullable=False)
